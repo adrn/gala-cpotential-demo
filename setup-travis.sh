c4a1ea8dd96add98fa9befa31d882f537ab2e31b
Adrian Price-Whelan <adrian.prw@gmail.com>
2017-03-20 17:07:35 -0400

    fix gala version

--- setup-travis.sh
+++ setup-travis.sh
@@ -2,9 +2,17 @@
 
 # http://conda.pydata.org/docs/travis.html#the-travis-yml-file
 if [[ "$TRAVIS_OS_NAME" == "osx" ]]; then
-  wget https://repo.continuum.io/miniconda/Miniconda3-latest-MacOSX-x86_64.sh -O miniconda.sh;
+  if [[ "$TRAVIS_PYTHON_VERSION" == "2.7" ]]; then
+    wget https://repo.continuum.io/miniconda/Miniconda2-latest-MacOSX-x86_64.sh -O miniconda.sh;
+  else
+    wget https://repo.continuum.io/miniconda/Miniconda3-latest-MacOSX-x86_64.sh -O miniconda.sh;
+  fi
 else
-  wget https://repo.continuum.io/miniconda/Miniconda3-latest-Linux-x86_64.sh -O miniconda.sh;
+  if [[ "$TRAVIS_PYTHON_VERSION" == "2.7" ]]; then
+    wget https://repo.continuum.io/miniconda/Miniconda2-latest-Linux-x86_64.sh -O miniconda.sh;
+  else
+    wget https://repo.continuum.io/miniconda/Miniconda3-latest-Linux-x86_64.sh -O miniconda.sh;
+  fi
 fi
 bash miniconda.sh -b -p $HOME/miniconda
 export PATH="$HOME/miniconda/bin:$PATH"
@@ -21,7 +29,7 @@ conda install -c conda-forge numpy=$NUMPY_VERSION cython pytest
 if [[ "$GALA_VERSION" == "dev" ]]; then
   pip install git+https://github.com/adrn/gala.git
 else
-  pip install astro-gala=$GALA_VERSION
+  pip install astro-gala
 fi
 
 # Build the extension
