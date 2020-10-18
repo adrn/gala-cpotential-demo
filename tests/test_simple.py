5e4d02a3fdc2afc45a90b84b9738a2674da9ef63
Adrian Price-Whelan <adrian.prw@gmail.com>
2020-10-18 10:51:23 -0400

    fix silly test name

--- tests/test_simple.py
+++ tests/test_simple.py
@@ -1,7 +1,8 @@
 from potentialdemo.potential import KeplerPotential
 import numpy as np
 
-def test_thing():
+
+def test_potentialdemo():
     pot = KeplerPotential(m=1.)
-    E = pot.energy([1.,0.1,-0.41])
+    E = pot.energy([1., 0.1, -0.41])
     assert np.all(np.isfinite(E))
